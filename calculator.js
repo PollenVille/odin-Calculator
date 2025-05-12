b1b5737da65016a6c13b649bc07cfe590fde10fb
PollenVille <169102796+PollenVille@users.noreply.github.com>
2025-05-12 17:52:31 -0400

    Code frontend Code Backend Code Styles

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const num1 = 0;
+const num2 = 0;
+const operator = '';
+
+function operate(num1, oper, num2) {
+    switch (oper) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
